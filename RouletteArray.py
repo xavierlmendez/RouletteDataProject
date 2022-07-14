5f699b827ba1428d4a0bf13689df08f9a565cf9c
Xavier Mendez <xluis10.2@gmail.com>
2022-07-14 16:21:58 -0500

    adding methods for RouletteArray.py and a class def

---
+++ RouletteArray.py
@@ -0,0 +1,12 @@
+class RA:
+    def __init__(self, name):
+        self.name = name
+        self.array = [[]]
+
+
+    def returnProb(self, entry):
+        print("returnProb in class RA activated: best solution would be posted")
+
+    def tally(self, entry):
+        print("tally in class RA has been activated: tally the entry")
+
