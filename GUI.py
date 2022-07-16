45eae837318495a31080ea079716f6149ba604ae
Xavier Mendez <xluis10.2@gmail.com>
2022-07-15 22:41:11 -0500

    Gui front end set up

--- GUI.py
+++ GUI.py
@@ -10,29 +10,37 @@ class MyFirstGUI:
                      "19", "20", "21", "22", "23", "24", "25", "26", "27",
                      "28", "29", "30", "31", "32", "33", "34", "35", "36"]
 
-    optionsLetter = [" ", 'B', 'R']
+    optionsColors = [" ", 'B', 'R']
     myGame = RA
 
     def __init__(self, master):
         self.master = master
         self.clickedNum = StringVar()
         self.clickedNum.set(0)
+        self.clickedColor = StringVar()
+        self.clickedColor.set(0)
         master.title("Roulette Probability GUI")
         master.geometry("400x350")
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text="please input roulette draw")
         self.label.pack()
 
-        self.drop = OptionMenu(master, self.clickedNum.get(), *MyFirstGUI.optionsNumber)
-        self.drop.pack()
+        self.drop_number = OptionMenu(master, self.clickedNum, *MyFirstGUI.optionsNumber)
+        self.drop_number.pack()
 
-        self.drop_button = Button(master, text="calculate probabilities", command=self.show)
+        self.drop_color = OptionMenu(master, self.clickedColor, *MyFirstGUI.optionsColors)
+        self.drop_color.pack()
+
+        self.drop_button = Button(master, text="calculate probabilities", command=self.showNum)
         self.drop_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    def show(self):
+    def showNum(self):
         self.label.config(text=self.clickedNum.get())
+
+    def showcolor(self):
+        self.label.config(text=self.clickedColor.get())
 
     def greet(self):
         print("Greetings!")
